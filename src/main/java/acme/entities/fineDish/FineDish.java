8418adf3ab03d2eff80eec6485e825d9eb49a619
antquiher1 <antquiher1@alum.us.es>
2022-08-10 10:48:35 +0200

    corrección de fallos menores

--- src/main/java/acme/entities/fineDish/FineDish.java
+++ src/main/java/acme/entities/fineDish/FineDish.java
@@ -8,7 +8,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
-import javax.validation.constraints.Future;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
--- src/main/java/acme/entities/partOf/PartOf.java
+++ src/main/java/acme/entities/partOf/PartOf.java
@@ -1,20 +1,13 @@
 package acme.entities.partOf;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-
-import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.URL;
 
 import acme.entities.recipe.Recipe;
 import acme.framework.entities.AbstractEntity;
-import acme.roles.Chef;
 import lombok.Getter;
 import lombok.Setter;
 
