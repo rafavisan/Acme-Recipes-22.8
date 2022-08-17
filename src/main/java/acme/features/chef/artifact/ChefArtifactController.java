b3b2371ab37528dc0e36d44bc31f8e6a0f6d292a
diecrequi <diecrequi@alum.us.es>
2022-08-17 21:02:42 +0200

    Tasks 08&09: Done

---
+++ src/main/java/acme/features/chef/artifact/ChefArtifactController.java
@@ -0,0 +1,37 @@
+package acme.features.chef.artifact;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import acme.entities.artifact.Artifact;
+import acme.framework.controllers.AbstractController;
+import acme.framework.roles.Any;
+import acme.roles.Chef;
+
+@Controller
+public class ChefArtifactController extends AbstractController<Chef, Artifact> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	protected ChefIngredientListService	listIngredientService;
+	
+	@Autowired
+	protected ChefUtensilListService	listUtensilService;
+	
+	@Autowired
+	protected ChefArtifactShowService showArtifactService;
+
+	// Constructors -----------------------------------------------------------
+
+
+	@PostConstruct
+	protected void initialise() {
+		super.addCommand("list-ingredient","list", this.listIngredientService);
+		super.addCommand("list-utensil","list", this.listUtensilService);
+		super.addCommand("show", this.showArtifactService);
+	}
+
+}
