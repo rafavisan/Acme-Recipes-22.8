89b5b1021c5abed35a623aabc8e099ef29783294
pedolirod <100024060+pedolirod@users.noreply.github.com>
2022-08-17 18:08:56 +0200

    Upload chef memorandum files

---
+++ src/main/java/acme/features/chef/memorandum/chefMemorandumController.java
@@ -0,0 +1,32 @@
+package acme.features.chef.memorandum;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import acme.entities.memorandum.Memorandum;
+import acme.framework.controllers.AbstractController;
+import acme.roles.Chef;
+
+@Controller
+public class chefMemorandumController extends AbstractController<Chef, Memorandum> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	protected chefMemorandumListService	listService;
+
+	@Autowired
+	protected chefMemorandumShowService	showService;
+
+	// Constructors -----------------------------------------------------------
+
+
+	@PostConstruct
+	protected void initialise() {
+		super.addCommand("list", this.listService);
+		super.addCommand("show", this.showService);
+	}
+
+}
