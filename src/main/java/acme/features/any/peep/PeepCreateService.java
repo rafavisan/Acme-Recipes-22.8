2b64809f99e31fdd64338e44a52512fe78dcaac2
antquiher1 <antquiher1@alum.us.es>
2022-09-03 18:53:43 +0200

    realización task 015 para peeps

--- src/main/java/acme/features/any/peep/PeepCreateService.java
+++ src/main/java/acme/features/any/peep/PeepCreateService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import acme.entities.peep.Peep;
+import acme.entities.systemSetting.SystemSettings;
 import acme.framework.components.models.Model;
 import acme.framework.controllers.Errors;
 import acme.framework.controllers.Request;
@@ -63,7 +64,27 @@ public class PeepCreateService implements AbstractCreateService<Any, Peep>{
 		
 		final Boolean isConfirmed = request.getModel().getBoolean("confirm");
 		errors.state(request, isConfirmed, "confirm", "any.peep.form.error.must-confirm");
+		
+		if(!errors.hasErrors("heading")) {
+			String[] text = entity.getHeading().toLowerCase().replaceAll("\n", " ").split("\\s+");
+			Double spamValue = checkSpam(text);
+			Double threshold = this.repository.findAllSpanTuples().getSpamThreshold();
+			errors.state(request, spamValue<threshold, "heading", "any.peep.error.heading.spam-threshold");
+		}
 
+		if(!errors.hasErrors("writer")) {
+			String[] text = entity.getWriter().toLowerCase().replaceAll("\n", " ").split("\\s+");
+			Double spamValue = checkSpam(text);
+			Double threshold = this.repository.findAllSpanTuples().getSpamThreshold();
+			errors.state(request, spamValue<threshold, "writer", "any.peep.error.writer.spam-threshold");
+		}
+		
+		if(!errors.hasErrors("text")) {
+			String[] text = entity.getText().toLowerCase().replaceAll("\n", " ").split("\\s+");
+			Double spamValue = checkSpam(text);
+			Double threshold = this.repository.findAllSpanTuples().getSpamThreshold();
+			errors.state(request, spamValue<threshold, "text", "any.peep.error.text.spam-threshold");
+		}
 	}
 
 	@Override
@@ -84,4 +105,35 @@ public class PeepCreateService implements AbstractCreateService<Any, Peep>{
 		this.repository.save(entity);
 	}
 
+	public Double checkSpam(String[] text) {
+		//--Initial data
+		SystemSettings ss = this.repository.findAllSpanTuples();
+		String[] strongWords = ss.getSpamTuples().toLowerCase().replaceAll("\\(", "").replaceAll(", ", ",").split("\\),");;
+		Integer nWord = 0;
+		Double spamValue= 0.;
+		nWord = text.length;
+		//--Method
+		for(int i = 0; i<strongWords.length;i++) {
+			String[] spamTuple = strongWords[i].toLowerCase().split(",");
+			String[] spamTerm = spamTuple[0].split(" ");
+			for(int o = 0; o<nWord; o++) {
+				boolean b = false;
+				int aux = nWord-spamTerm.length+1;
+				for(int p = 0; p<spamTerm.length; p++) {
+					b = true;
+					
+					if(!spamTerm[p].equals(text[o+p])) {
+						b=false;
+						break;
+					}
+				}
+				if(b) {
+					Double spamTermWeight = Double.parseDouble(spamTuple[1]);
+					spamValue += spamTermWeight/aux;
+				}
+			}
+		}
+		return spamValue;
+	}
+	
 }
