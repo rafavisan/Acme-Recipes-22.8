64502855b04425437991458f15f9001e130be8ee
antquiher1 <antquiher1@alum.us.es>
2022-09-04 19:40:58 +0200

    corrección botón administrator y spam validator for bulletin

--- src/main/java/acme/features/administrator/bulletin/AdministratorBulletinCreateService.java
+++ src/main/java/acme/features/administrator/bulletin/AdministratorBulletinCreateService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import acme.entities.bulletin.Bulletin;
+import acme.entities.systemSetting.SystemSettings;
 import acme.features.authenticated.bulletin.AuthenticatedBulletinRepository;
 import acme.framework.components.models.Model;
 import acme.framework.controllers.Errors;
@@ -63,6 +64,19 @@ public class AdministratorBulletinCreateService implements AbstractCreateService
 
 		confirmation = request.getModel().getBoolean("confirmation");
 		errors.state(request, confirmation, "confirmation", "administrator.bulletin.form.label.confirmation.message");
+		
+		if(!errors.hasErrors("heading")) {
+			String[] text = entity.getHeading().toLowerCase().replaceAll("\n", " ").split("\\s+");
+			Double spamValue = checkSpam(text);
+			Double threshold = this.repository.findAllSpanTuples().getSpamThreshold();
+			errors.state(request, spamValue<threshold, "heading", "chef.bulletin.error.heading.spam-threshold");
+		}
+		if(!errors.hasErrors("text")) {
+			String[] text = entity.getText().toLowerCase().replaceAll("\n", " ").split("\\s+");
+			Double spamValue = checkSpam(text);
+			Double threshold = this.repository.findAllSpanTuples().getSpamThreshold();
+			errors.state(request, spamValue<threshold, "text", "chef.bulletin.error.text.spam-threshold");
+		}
 	}
 
 	@Override
@@ -83,4 +97,37 @@ public class AdministratorBulletinCreateService implements AbstractCreateService
 		this.repository.save(entity);
 	}
 
+	public Double checkSpam(String[] text) {
+		//--Initial data
+		SystemSettings ss = this.repository.findAllSpanTuples();
+		String[] strongWords = ss.getSpamTuples().toLowerCase().replaceAll("\\(", "").replaceAll(", ", ",").split("\\),");;
+		Integer nWord = 0;
+		Double spamValue= 0.;
+		nWord = text.length;
+		//--Method
+		for(int i = 0; i<strongWords.length;i++) {
+			String[] spamTuple = strongWords[i].toLowerCase().split(",");
+			String[] spamTerm = spamTuple[0].split(" ");
+			for(int o = 0; o<nWord; o++) {
+				boolean b = false;
+				int aux = nWord-spamTerm.length+1;
+				if(aux>0) {
+					for(int p = 0; p<spamTerm.length; p++) {
+						b = true;
+						
+						if(!spamTerm[p].equals(text[o+p])) {
+							b=false;
+							break;
+						}
+					}
+					if(b) {
+						Double spamTermWeight = Double.parseDouble(spamTuple[1]);
+						spamValue += spamTermWeight/aux;
+					}
+				}
+			}
+		}
+		return spamValue;
+	}
+	
 }
